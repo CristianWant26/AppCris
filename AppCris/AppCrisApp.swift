0f76da83611367aba47adf39114ffd1bd94bb9cb
Cristian Gómez <yair.gomher26@gmail.com>
2024-09-04 16:18:07 -0600

    Initial Commit

---
+++ AppCris/AppCrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCrisApp.swift
+//  AppCris
+//
+//  Created by Cristian Gómez  on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AppCrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
